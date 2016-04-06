f46e23a0a46d28376905720a843ac0e1abeb5fee
jekabutov1985 <jbutov@mail.ru>
2016-04-06 21:15:42 +0300

    Add js7-8

---
+++ js 7-8/js/script.js
@@ -0,0 +1,28 @@
+﻿
+$(function () {
+/*Tabs*/
+	
+	var $links = $('.menu a');
+	$links.on('click', function(e) {
+		e.preventDefault();
+		$links.removeClass('active');
+		$(this).addClass('active');
+		var tab = $(this).attr('href');
+		$('.tab').not(tab).hide();
+		$(tab).fadeIn(400)
+	});
+
+/*Tooltips*/
+
+    $('.aside_box').hover(
+		function(){
+		$(this).find('span').stop().animate({opacity: 1});
+		},
+		function(){
+		$(this).find('span').stop().animate({opacity: 0});
+		}),
+	
+	$('.help_button').click(function() {
+		$('.aside_box span').stop().animate({opacity: 1});
+	});
+})
