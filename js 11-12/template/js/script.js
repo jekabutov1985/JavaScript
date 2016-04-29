b710df0a8d88a684d743cee60594d297c37c80ea
jekabutov1985 <jbutov@mail.ru>
2016-04-29 19:16:51 +0300

    Add js 11-12

---
+++ js 11-12/template/js/script.js
@@ -0,0 +1,36 @@
+﻿'use strict'
+$(function(){
+
+	var html = $('#resume').html();
+
+	console.log(html);
+
+	var myResume = [
+			{	
+				name: 'Бутов Евгений Александрович',
+				img: './img/foto1.jpg',
+				info: 'Студент курса GoFrontend GoFE 4'
+			},
+			{	
+				reasons: ['Заборы строить не интересно',
+				'Мало платят',
+				'Приходится работать по ночам']
+			},
+			{
+			
+				tel: '050-050-50-50',
+				vk: 'https://vk.com/id86291584'
+			},
+			{
+			
+				feedback:'Еслу нужно,могу построить вам забор'
+			}
+		];
+
+	var content = tmpl(html,{
+		data: myResume
+	});
+
+	$('body').append(content);
+
+});
