45f9030009670c229c2d6879f8d3e4d1c5feec8c
jekabutov1985 <jbutov@mail.ru>
2016-05-28 20:22:45 +0300

    Add js 17-18

---
+++ js 17-18/js/src/QBase.js
@@ -0,0 +1,38 @@
+﻿'use strict'
+var QBase = {
+	title: 'Белиберда',
+	prompts: '(Кликаем правильный вариант)',
+	question: [
+		{
+			text: 'Кто Сильнее?',
+			answer: [
+				{text: 'Брюс Ли', check: false},
+				{text: 'Чак Норрис', check: true},
+				{text: 'Человек паук', check: false},
+			]
+		},	
+		
+		{
+			text: 'Что делать если хотел вытереть девушке слёзы, но случайно стёр брови?',
+			answer: [
+				{text: 'Сразу же оценить её красоту без бровей', check: false},
+				{text: 'Рассказать анекдот', check: false},
+				{text: 'Бежать', check: true},
+			]
+		},	
+		
+		{
+			text: 'Как не привлекать внимание продавца-консультанта?',
+			answer: [
+				{text: 'Не заходить в магазин', check: false},
+				{text: 'Не прикрываясь чихать и судорожно чесаться', check: false},
+				{text: 'Это не возможно,они всегда спешат на помощь', check: true},
+			]
+		},	
+	]	
+};	
+
+	localStorage.setItem('QBase',JSON.stringify(QBase));
+	var QBase = localStorage.getItem('QBase');
+	QBase = JSON.parse(QBase);
+	
